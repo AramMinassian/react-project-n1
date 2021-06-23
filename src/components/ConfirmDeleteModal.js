afb27c1b34a6ed037cf92514298092badd52f83a
Aram Minasyan <aramminasyan@Arams-MacBook-Air.local>
2021-06-23 14:59:54 +0400

    minor changes

--- src/components/ConfirmDeleteModal.js
+++ src/components/ConfirmDeleteModal.js
@@ -1,23 +1,21 @@
 import React from "react";
+import {Button} from "react-bootstrap";
+import "./ConfirmDeleteModal.css";
 
 class Modal extends React.Component {
   render() {
     return (
-      <div className="modal-wrapper-1">
-        <div className="modal-header-1">
-          <p>Modal header text</p>
-          <span className="close-modal-btn">x</span>
+      <div className="cdm-wrapper">
+        <div className="cdm-header">
+          <p>Delete tasks</p>
+          <span className="cdm-close-btn">x</span>
         </div>
-        <div className="modal-body-1">
-          <h4>Title</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Perferendis quibusdam itaque perspiciatis laudantium architecto non? 
-            Ipsa consequatur nobis sequi culpa, cupiditate eveniet neque aut 
-            ipsum repudiandae cum laborum ipsam illo.</p>
+        <div className="cdm-body">
+          <p>Are you sure you want to delete selected task?</p>
         </div>
-        <div className="modal-footer-1">
-          <button className="btn-cancel">Cancel</button>
-          <button className="btn-done">Done</button>
+        <div className="cdm-footer">
+          <Button variant="primary">Cancel</Button>
+          <Button variant="danger">Delete</Button>
         </div>
       </div>
     )
