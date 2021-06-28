35d1cdb08eb447dd6ee39ac1f5285b2af4403fc5
Aram Minasyan <aramminasyan@Arams-MacBook-Air.local>
2021-06-29 02:33:26 +0400

    react-router used, app pages and nav menu created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import "./App.css";
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import NavMenu from "./components/NavMenu";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 
-function App(){
+function App() {
     return (
         <div className="App">
-            <h1>Create to-do list, be more productive</h1>
-            <Home />
+            <Router>
+                <NavMenu />
+                <Switch>
+                    <Route 
+                        path="/"
+                        component={Home}
+                        exact={true}
+                    />
+                    <Route 
+                        path="/home"
+                        component={Home}
+                        exact={true}
+                    />
+                    <Route 
+                        path="/about"
+                        component={About}
+                        exact={true}
+                    />
+                    <Route 
+                        path="/contact"
+                        component={Contact}
+                        exact={true}
+                    />
+                    <Route 
+                        path="/not-found"
+                        component={NotFound}
+                        exact={true}
+                    />
+                    <Redirect 
+                        to="/not-found"
+                    />
+                </Switch>
+            </Router>
         </div>
-        
+
     )
 }
 
