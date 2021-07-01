84f9b9a4752d08e0fdccb4b82863b60ebbe9df07
Aram Minasyan <aramminasyan@Arams-MacBook-Air.local>
2021-07-02 03:55:03 +0400

    contact page finished

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,7 @@
 import "./Contact.css";
 import React from "react";
 import { Button, FormControl } from "react-bootstrap";
+import validator from "validator";
 
 
 class Contact extends React.Component {
@@ -12,9 +13,9 @@ class Contact extends React.Component {
             message: "",
         },
         formError: {
-            name: "",
-            email: "",
-            message: "",
+            name: false,
+            email: false,
+            message: false,
         }
     }
 
@@ -26,36 +27,113 @@ class Contact extends React.Component {
                 [name]: value
             }
         });
+
+        if (name === "email") {
+            this.setState({
+                formError: {
+                    ...this.state.formError,
+                    email: !validator.isEmail(value)
+                }
+            })
+        } else {
+            this.setState({
+                formError: {
+                    ...this.state.formError,
+                    [name]: !value
+                }
+            })
+        }
+    }
+
+    handleSend = () => {
+
+        const { formData } = this.state;
+        const formError = {
+            name: !formData.name,
+            email: !validator.isEmail(formData.email),
+            message: !formData.message
+        }
+        this.setState({ formError });
+        for (let error in formError) {
+            if (formError[error]) return;
+        }
+
+        fetch("http://localhost:3001/form", {
+            method: "POST",
+            body: JSON.stringify(formData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(async (response) => {
+                const result = await response.json();
+                if (response.status >= 400 && response.status < 600) {
+                    if (result.error) {
+                        throw result.error;
+                    } else {
+                        throw new Error("Something went wrong");
+                    }
+                }
+                this.setState({
+                    formData: {
+                        name: "",
+                        email: "",
+                        message: ""
+                    }
+                })
+            })
+
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     render() {
+        const { formError, formData } = this.state;
         return (
             <div className="Contact">
                 <h1>Contact us</h1>
                 <div className="contact-form">
-                    <FormControl
-                        className="field"
-                        placeholder="Name"
-                        name="name"
-                        onChange={this.handleChange}
-                    />
-                    <FormControl
-                        className="field"
-                        placeholder="Email"
-                        name="email"
-                        onChange={this.handleChange}
-                    />
-                    <FormControl
-                        className="field"
-                        as="textarea"
-                        rows={7}
-                        placeholder="Message"
-                        name="message"
-                        onChange={this.handleChange}
-                    />
+                    <div className="field">
+                        <FormControl
+                            placeholder="Name"
+                            name="name"
+                            value={formData.name}
+                            onChange={this.handleChange}
+                        />
+                        {formError.name && <small
+                            className="error"
+                        >Please provide a name</small>}
+                    </div>
+                    <div className="field">
+                        <FormControl
+                            type="email"
+                            placeholder="Email"
+                            name="email"
+                            value={formData.email}
+                            onChange={this.handleChange}
+                        />
+                        {formError.email && <small
+                            className="error"
+                        >Please provide a formerly formatted email address</small>}
+                    </div>
+                    <div className="field">
+                        <FormControl
+                            as="textarea"
+                            rows={7}
+                            placeholder="Message"
+                            name="message"
+                            value={formData.message}
+                            onChange={this.handleChange}
+                        />
+                        {formError.message && <small
+                            className="error"
+                        >Please provide a message</small>}
+                    </div>
                     <Button
                         className="btn"
                         variant="primary"
+                        onClick={this.handleSend}
                     >SEND</Button>
                 </div>
             </div>
